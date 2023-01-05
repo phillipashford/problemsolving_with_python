c59e65377dfc3e838c5e338ae5ac56012612cb4d
Phillip  Ashford <phillip.ashford@uky.edu>
2023-01-05 12:17:22 -0500

    Create while_loop_counter.py

---
+++ while_loop_counter.py
@@ -0,0 +1,34 @@
+#################################
+# Pseudocode
+
+# Initialize variable to hold range value
+# Initialize counter to control and exit while loop when complete
+# Write while loop that tracks the value of the counter
+    # Write for loop with range function assigned to variable
+        # Print output
+    # Print to new line after each iteration of the for loop
+    # Reduce the value in the range variable by 1 (range -= 1) for the next loop iteration
+    # Reduce the counter by 1 for the next loop iteration
+
+#################################
+
+# Initialize variable to hold range value
+range_value = 10
+
+# Initialize counter to control and exit while loop when complete
+counter = 9
+
+# Write while loop that tracks the value of the counter
+while counter < 10 and counter > -1:
+    # Write for loop with range function assigned to variable
+    for num in range(range_value):
+        # Print output
+        print(num, end='')
+    # Print to new line for desired output
+    print()
+
+    # Reduce the value in the range variable by 1 for the next loop iteration
+    range_value -= 1
+
+    # Reduce the counter by 1 for the next loop iteration
+    counter -= 1
