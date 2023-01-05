b9c62cc503ac1add1272f35c4b2e3649eeee8e0d
Phillip  Ashford <phillip.ashford@uky.edu>
2023-01-05 12:18:23 -0500

    Create conversion functions folder

---
+++ conversion_functions/temps.py
@@ -0,0 +1,7 @@
+def c_to_f(celsius_temp):
+    fahrenheit = (celsius_temp * 9/5) + 32
+    print(f'{celsius_temp} Celsius equals {fahrenheit:.1f} Fahrenheit')
+
+def f_to_c(fahrenheit_temp):
+    celsius = (fahrenheit_temp - 32) * 5/9
+    return celsius
